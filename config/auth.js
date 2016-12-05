6fd59f54e83017afa8aca43ff6dc73229dc625cb
roshanKadasani <roshan_123@csu.fullerton.edu>
2016-12-05 01:50:57 -0800

    changed twitter callback url

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
     'twitterAuth' : {
       'consumerKey' : 'ODYUi2tZ8Vcs8bfgR1Vko57XO',
       'consumerSecret' : 'uLjy2AmG9VhhJcCyF65P1OOFWDMl0j0iCjXe4UCVzDePPEMWZE',
-      'callbackURL' : 'https://projectdare.herokuapp.com/dashboard',
+      'callbackURL' : 'https://projectdare.herokuapp.com/auth/twitter/callback',
       'accessToken' :    '1331699466-jQbGzEqcHkZXnr4AuWMuZzW8AxbM5vZh7NChOPi',
       'accessTokenSecret':  'aH14SJuBhatSqVUWvqxE4DUxHJJ6FgGLE8AC6ZT3rJ5Q2'
     }
