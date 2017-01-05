0131195f022ba8ba88bc54bbbc241d2978a07cc0
Roshan Kadasani <roshan_123@csu.fullerton.edu>
2017-01-05 10:40:54 -0800

    updated keys

--- config/auth.js
+++ config/auth.js
@@ -1,9 +1,9 @@
 module.exports = {
     'twitterAuth' : {
-      'consumerKey' : 'FUbLJmh9L4szcej0virRxI9ow',
-      'consumerSecret' : '6mOsakxexCegwxX1kQOBgL6cnB747AKmejvAS9pJ3RFbZ6nFHg',
+      'consumerKey' : '13Gcf2lw7dMdesFeTsu4oisTO',
+      'consumerSecret' : 'GNDKk2aQBeLqSi3Mi95LIM9aOhFS8NiP8ZTH0WLQm2nahm6liG',
       'callbackURL' : 'https://projectdare.herokuapp.com/auth/twitter/callback',
-      'accessToken' :    '1331699466-ChWli7qCFrlniZVsAuY1DOt2w6LXBOBccD1WLy0',
-      'accessTokenSecret':  'FKsPEbMiQ2iciOk4UFVBOz229eFGoTMzZ0ibmC9nItSvA'
+      'accessToken' :    '1331699466-93GgNkTqifjzWCmFI3PeozEgeqfX200XUbiGV37',
+      'accessTokenSecret':  '7r5OTXfZuhofIgn2JJ4WXjZGM8029rIKgJcDmoBWPf04g'
     }
 };
